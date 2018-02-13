08307b18941ba4039a8d82b176c179b99fac4f42
guenengafo <freddy_guenengafo@yahoo.fr>
2018-02-13 14:56:21 +0100

    Personnalisation page de contact (3)

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -2,31 +2,41 @@
 
 
 @section('content')
-		<h2>Nous contacter ?</h2>
 
-		  	<form action="#" method="POST" class="card bg-light p-3 col-md-6">
-		  		{{--Name field--}}
-		  		<div class="form-group">
-		  			<label for="name" class="control-label">Nom</label>
-		  			<input type="text" name="name" id="name" class="form-control" required="required">
-		  		</div>
+	<h2>Nous contacter ?</h2>
 
-		  		{{--Email field--}}
-		  		<div class="form-group">
-		  			<label for="email" class="control-label">Email</label>
-		  			<input type="email" name="email" id="email" class="form-control" required="required">
-		  		</div>
+		<div class="row">
+			<div class="col-md-6">
+		  		<form action="#" method="POST" class="card bg-light p-3">
+			  		{{--Name field--}}
+			  		<div class="form-group">
+			  			<label for="name" class="control-label">Nom</label>
+			  			<input type="text" name="name" id="name" class="form-control" required="required">
+			  		</div>
 
-		  		{{--Message field--}}
-		  		<div class="form-group">
-		  			<label for="message" class="control-label">Message</label>
-		  			<textarea class="form-control" rows="5" cols="10" required="required" 
-		  			name="message" id="message"></textarea>
-		  		</div>
+			  		{{--Email field--}}
+			  		<div class="form-group">
+			  			<label for="email" class="control-label">Email</label>
+			  			<input type="email" name="email" id="email" class="form-control" required="required">
+			  		</div>
 
-		  		{{--Button--}}
-		  		<div class="form-group">
-		  			<button class="btn btn-primary">Envoyer</button>
-		  		</div>
-		  	</form>
+			  		{{--Message field--}}
+			  		<div class="form-group">
+			  			<label for="message" class="control-label">Message</label>
+			  			<textarea class="form-control" rows="5" cols="10" required="required" 
+			  			name="message" id="message"></textarea>
+			  		</div>
+
+			  		{{--Button--}}
+			  		<div class="form-group">
+			  			<button class="btn btn-primary">Envoyer</button>
+			  		</div>
+		  		</form>
+		  	</div>
+		  	<div class="col-md-6">
+		  		<h2><center>Ou via nos réseaux sociaux</center></h2>
+		  		On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web...
+		  	</div>
+		</div>
+
 @stop
